8d4df7cb0d2df7e659ced8c019e20b3f5d271f1b
Anshuman Sinha <anshumansinha2001@gmail.com>
2024-07-28 16:01:35 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "res.cloudinary.com",
+      "anshumansinha2001.github.io",
+    ],
+  },
+};
 
 export default nextConfig;
